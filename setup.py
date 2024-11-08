ff9351216eb8371d887d09f1e758cec74675030b
Aaron Wienkers <aaron.wienkers@gmail.com>
2024-11-08 16:22:50 +0100

    Skeleton for splitting and merging framework

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='spot_the_blOb',
+    use_scm_version={
+        "write_to": "spot_the_blOb/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    }
+)
---
+++ spot_the_blOb/__init__.py
@@ -0,0 +1,1 @@
+from .spot_the_blOb import spot_the_blOb
